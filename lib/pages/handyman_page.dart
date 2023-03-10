e000cbd673a491b0f7b5a4f2d75d3db3696c95d7
Arnau <>
2023-03-10 21:43:32 +0100

    feat: add bottom navigation menu

---
+++ lib/pages/handyman_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HandymanPage extends StatelessWidget {
+  const HandymanPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
