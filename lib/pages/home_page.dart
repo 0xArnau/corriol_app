51d5035f11bfa0d15e1f92a2db351107aac9585a
Arnau <>
2023-03-11 22:31:18 +0100

    feat: add logic for GPS

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:corriol_app/classes/geolocation_class.dart';
 import 'package:corriol_app/core/constants.dart';
+import 'package:corriol_app/core/notifiers.dart';
 import 'package:corriol_app/pages/home/contact_page.dart';
 import 'package:corriol_app/pages/home/corriol_page.dart';
 import 'package:corriol_app/pages/home/my_records_page.dart';
@@ -12,67 +14,91 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            GridView.count(
-              crossAxisCount: 2,
-              childAspectRatio: 1,
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
+    return ValueListenableBuilder(
+      valueListenable: isGpsOnNotifier,
+      builder: (context, isGpsOn, child) {
+        return Scaffold(
+          body: SingleChildScrollView(
+            child: Column(
               children: [
-                CardWidget(
-                  imagePath: 'null', // TODO: Changes this to a valid image path
-                  text: AppLocalizations.of(context).meetKentishPlover,
-                  background: kColorRedCard,
-                  foreground: Colors.black,
-                  page: const CorriolPage(),
+                GridView.count(
+                  crossAxisCount: 2,
+                  childAspectRatio: 1,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: [
+                    CardWidget(
+                      imagePath:
+                          'null', // TODO: Changes this to a valid image path
+                      text: AppLocalizations.of(context).meetKentishPlover,
+                      background: kColorRedCard,
+                      foreground: Colors.black,
+                      page: const CorriolPage(),
+                    ),
+                    CardWidget(
+                      imagePath:
+                          'null', // TODO: Changes this to a valid image path
+                      text: AppLocalizations.of(context).contact,
+                      background: kColorPurpleCard,
+                      foreground: Colors.black,
+                      page: const ContactPage(),
+                    ),
+                    CardWidget(
+                      imagePath:
+                          'null', // TODO: Changes this to a valid image path
+                      text: AppLocalizations.of(context).recordObservation,
+                      background: kColorGreenCard,
+                      foreground: Colors.black,
+                      page: const RecordObservationPage(),
+                    ),
+                    CardWidget(
+                      imagePath:
+                          'null', // TODO: Changes this to a valid image path
+                      text: AppLocalizations.of(context).myRecords,
+                      background: kColorYellowCard,
+                      foreground: Colors.black,
+                      page: const MyRecordsPage(),
+                    ),
+                  ],
                 ),
-                CardWidget(
-                  imagePath: 'null', // TODO: Changes this to a valid image path
-                  text: AppLocalizations.of(context).contact,
-                  background: kColorPurpleCard,
-                  foreground: Colors.black,
-                  page: const ContactPage(),
+                const SizedBox(height: kDoublePaddingGridViewText),
+                Text(
+                  AppLocalizations.of(context).warningText112,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: kColorPrimaryGrey,
+                  ),
                 ),
-                CardWidget(
-                  imagePath: 'null', // TODO: Changes this to a valid image path
-                  text: AppLocalizations.of(context).recordObservation,
-                  background: kColorGreenCard,
-                  foreground: Colors.black,
-                  page: const RecordObservationPage(),
-                ),
-                CardWidget(
-                  imagePath: 'null', // TODO: Changes this to a valid image path
-                  text: AppLocalizations.of(context).myRecords,
-                  background: kColorYellowCard,
-                  foreground: Colors.black,
-                  page: const MyRecordsPage(),
-                ),
+                const Text(
+                  '112',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 60,
+                    fontWeight: FontWeight.bold,
+                    color: kColorPrimaryBlue,
+                  ),
+                )
+                // TODO: Add GEPEC & EdC Logos
               ],
             ),
-            const SizedBox(height: kDoublePaddingGridViewText),
-            Text(
-              AppLocalizations.of(context).warningText112,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                color: kColorPrimaryGrey,
-              ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              isGpsOnNotifier.value
+                  ? GeolocationClass().disableLocationPermission()
+                  : GeolocationClass().enableLocationPermission();
+            },
+            child: ValueListenableBuilder(
+              valueListenable: isGpsOnNotifier,
+              builder: (context, isGpsOn, child) {
+                return isGpsOn
+                    ? const Icon(Icons.gps_fixed)
+                    : const Icon(Icons.gps_off);
+              },
             ),
-            const Text(
-              '112',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 60,
-                fontWeight: FontWeight.bold,
-                color: kColorPrimaryBlue,
-              ),
-            )
-            // TODO: Add GEPEC & EdC Logos
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
