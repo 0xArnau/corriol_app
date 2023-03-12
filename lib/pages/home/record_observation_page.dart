beacd41bf5d8068aef3a300d2bdc12720ba2500c
Arnau <>
2023-03-12 15:14:38 +0100

    fix: run CircularProgressIndicator only when GPS permission is not granted

--- lib/pages/home/record_observation_page.dart
+++ lib/pages/home/record_observation_page.dart
@@ -1,5 +1,6 @@
 import 'package:corriol_app/classes/geolocation_class.dart';
 import 'package:corriol_app/core/constants.dart';
+import 'package:corriol_app/core/notifiers.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
@@ -22,8 +23,10 @@ class _RecordObservationPageState extends State<RecordObservationPage> {
 
   Future<void> updateLocation() async {
     GeolocationClass().enableLocationPermission();
-    await Future.delayed(
-        const Duration(seconds: 5)); // TODO: Change this to a better solution
+    if (!isGpsOnNotifier.value) {
+      await Future.delayed(
+          const Duration(seconds: 5)); // TODO: Change this to a better solution
+    }
     Position? newPosition = await GeolocationClass().getCurrentLocation();
     setState(() {
       _position = newPosition;
