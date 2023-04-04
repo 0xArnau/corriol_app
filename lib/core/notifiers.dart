140e5070aca7734bcabd4d6ef01ac85769e2d241
Arnau <>
2023-04-04 19:37:29 +0200

    feat: show the current position (longitude & latitude)
    TODO: change the coordinates to the name of the place

--- lib/core/notifiers.dart
+++ lib/core/notifiers.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 ValueNotifier<bool> isGpsOnNotifier = ValueNotifier(false);
+ValueNotifier<LatLng> currenPositionNotifier =
+    ValueNotifier(const LatLng(41.8205, 1.8401)); // Catalunya
