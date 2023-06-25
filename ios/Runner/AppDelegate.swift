c44c15f26621846550200ac26b94497ef5388a75
Arnau <>
2023-06-25 12:33:40 +0200

    feat: add .env file for iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,14 +1,16 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import flutter_config
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("GOOGLE_MAPS_API_KEY_IOS")
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey(FlutterConfigPlugin.env(for: "GOOGLE_MAPS_API_KEY_IOS"))
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
