5c8f15ed1cd3c1c1569187e75766c13ed839a2d9
Arnau <>
2023-07-15 17:09:32 +0200

    refactor(home page): removed unused code

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,4 @@
-import 'package:corriol_app/classes/geolocation_class.dart';
 import 'package:corriol_app/core/constants.dart';
-import 'package:corriol_app/core/notifiers.dart';
 import 'package:corriol_app/pages/home/contact_page.dart';
 import 'package:corriol_app/pages/home/corriol_page.dart';
 import 'package:corriol_app/pages/home/my_records_page.dart';
@@ -14,94 +12,69 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      valueListenable: isGpsOnNotifier,
-      builder: (context, isGpsOn, child) {
-        return Scaffold(
-          body: SingleChildScrollView(
-            child: Column(
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            GridView.count(
+              crossAxisCount: 2,
+              childAspectRatio: 1,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
               children: [
-                GridView.count(
-                  crossAxisCount: 2,
-                  childAspectRatio: 1,
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  children: [
-                    CardWidget(
-                      imagePath:
-                          'assets/images/home/1_1-coneix-al-corriol.png', // TODO: Changes this to a valid image path
-                      text: AppLocalizations.of(context).meetKentishPlover,
-                      background: kColorRedCard,
-                      foreground: Colors.black,
-                      page: const CorriolPage(),
-                    ),
-                    CardWidget(
-                      imagePath:
-                          'assets/images/home/1_6-contacta.png', // TODO: Changes this to a valid image path
-                      text: AppLocalizations.of(context).contact,
-                      background: kColorPurpleCard,
-                      foreground: Colors.black,
-                      page: ContactPage(),
-                    ),
-                    CardWidget(
-                      imagePath:
-                          'assets/images/home/1_3-anotar-observacio.png', // TODO: Changes this to a valid image path
-                      text: AppLocalizations.of(context).recordObservation,
-                      background: kColorGreenCard,
-                      foreground: Colors.black,
-                      page: const RecordObservationPage(),
-                    ),
-                    CardWidget(
-                      imagePath:
-                          'assets/images/home/1_4-registres.png', // TODO: Changes this to a valid image path
-                      text: AppLocalizations.of(context).myRecords,
-                      background: kColorYellowCard,
-                      foreground: Colors.black,
-                      page: MyRecordsPage(),
-                    ),
-                  ],
+                CardWidget(
+                  imagePath: 'assets/images/home/1_1-coneix-al-corriol.png',
+                  text: AppLocalizations.of(context).meetKentishPlover,
+                  background: kColorRedCard,
+                  foreground: Colors.black,
+                  page: const CorriolPage(),
                 ),
-                const SizedBox(height: kDoublePaddingGridViewText),
-                Text(
-                  AppLocalizations.of(context).warningText112,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    color: kColorPrimaryGrey,
-                  ),
+                CardWidget(
+                  imagePath: 'assets/images/home/1_6-contacta.png',
+                  text: AppLocalizations.of(context).contact,
+                  background: kColorPurpleCard,
+                  foreground: Colors.black,
+                  page: ContactPage(),
                 ),
-                const Text(
-                  '112',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 60,
-                    fontWeight: FontWeight.bold,
-                    color: kColorPrimaryBlue,
-                  ),
+                CardWidget(
+                  imagePath: 'assets/images/home/1_3-anotar-observacio.png',
+                  text: AppLocalizations.of(context).recordObservation,
+                  background: kColorGreenCard,
+                  foreground: Colors.black,
+                  page: const RecordObservationPage(),
                 ),
-                // TODO: Add GEPEC & EdC Logos
-                const Image(
-                  image: AssetImage('assets/images/GEPEC_EdC_OFICIAL.png'),
+                CardWidget(
+                  imagePath: 'assets/images/home/1_4-registres.png',
+                  text: AppLocalizations.of(context).myRecords,
+                  background: kColorYellowCard,
+                  foreground: Colors.black,
+                  page: const MyRecordsPage(),
                 ),
               ],
             ),
-          ),
-          // floatingActionButton: FloatingActionButton(
-          //   onPressed: () {
-          //     isGpsOnNotifier.value
-          //         ? GeolocationClass().disableLocationPermission()
-          //         : GeolocationClass().enableLocationPermission();
-          //   },
-          //   child: ValueListenableBuilder(
-          //     valueListenable: isGpsOnNotifier,
-          //     builder: (context, isGpsOn, child) {
-          //       return isGpsOn
-          //           ? const Icon(Icons.gps_fixed)
-          //           : const Icon(Icons.gps_off);
-          //     },
-          //   ),
-          // ),
-        );
-      },
+            const SizedBox(height: kDoublePaddingGridViewText),
+            Text(
+              AppLocalizations.of(context).warningText112,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: kColorPrimaryGrey,
+              ),
+            ),
+            const Text(
+              '112',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 60,
+                fontWeight: FontWeight.bold,
+                color: kColorPrimaryBlue,
+              ),
+            ),
+            const Image(
+              image: AssetImage('assets/images/GEPEC_EdC_OFICIAL.png'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
