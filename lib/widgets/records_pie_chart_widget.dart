572582b4314b389618a69b369fa70e1461a329cc
Arnau <>
2023-07-15 17:23:49 +0200

    refactor(report chart): change the way to get the reports
    Only get the firestore reports
    
    TODO: before getting the reports, save the local reports

--- lib/widgets/records_pie_chart_widget.dart
+++ lib/widgets/records_pie_chart_widget.dart
@@ -1,121 +1,122 @@
-import 'package:corriol_app/controllers/auth_controller.dart';
 import 'package:corriol_app/controllers/report_controller.dart';
-import 'package:corriol_app/core/notifiers.dart';
 import 'package:corriol_app/models/report_model.dart';
 import 'package:corriol_app/core/constants.dart';
+import 'package:corriol_app/models/user_model.dart';
+import 'package:corriol_app/models/user_preferences_model.dart';
+import 'package:corriol_app/providers/user_provider.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class RecordsPieChartWidget extends StatefulWidget {
+class RecordsPieChartWidget extends StatelessWidget {
   const RecordsPieChartWidget({super.key, required this.species});
 
   final Species species;
 
   @override
-  State<RecordsPieChartWidget> createState() => _RecordsPieChartWidgetState();
-}
-
-class _RecordsPieChartWidgetState extends State<RecordsPieChartWidget> {
-  late Future<List<ReportModel>> records;
-
-  @override
-  void initState() {
-    super.initState();
-
-    print("is connected? ${isConnectedNotifier.value}");
-
-    if (isConnectedNotifier.value) {
-      records = ReportController()
-          .getReportsByUserId(AuthController().currentUser!.email!);
-    } else {
-      records = ReportModel.loadRecords();
-    }
-    print("records ${records}");
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<ReportModel>>(
-      future: records,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          var femelles = 0;
-          var polls = 0;
-          var mascles = 0;
-          var indeterminat = 0;
-          var gossos = 0;
-          var gats = 0;
-          final _records = snapshot.data!;
-
-          for (final element in _records) {
-            print(element.toJson());
-
-            if (element.species != widget.species) continue;
-
-            femelles += element.females;
-            polls += element.chickens;
-            mascles += element.males;
-            indeterminat += element.undetermined;
-            gossos += element.dogs;
-            gats += element.cats;
-          }
-
-          print("femelles ${femelles}");
-          print("polls ${polls}");
-          print("mascles ${mascles}");
-          print("indeterminats ${indeterminat}");
-          print("gossos ${gossos}");
-          print("gats ${gats}");
-
-          final data = [
-            PieChartSectionData(
-              value: femelles.toDouble(),
-              color: kColorList['femelles'],
-              radius: 50,
-            ),
-            PieChartSectionData(
-              value: polls.toDouble(),
-              color: kColorList['polls'],
-              radius: 50,
-            ),
-            PieChartSectionData(
-              value: mascles.toDouble(),
-              color: kColorList['mascles'],
-              radius: 50,
-            ),
-            PieChartSectionData(
-              value: indeterminat.toDouble(),
-              color: kColorList['indeterminat'],
-              radius: 50,
-            ),
-            PieChartSectionData(
-              value: gossos.toDouble(),
-              color: kColorList['gossos'],
-              radius: 50,
-            ),
-            PieChartSectionData(
-              value: gats.toDouble(),
-              color: kColorList['gats'],
-              radius: 50,
-            ),
-          ];
-
-          return SizedBox(
-            width: 300,
-            height: 300,
-            child: PieChart(
-              PieChartData(
-                sections: data,
-              ),
-            ),
-          );
-        } else if (snapshot.hasError) {
-          return const Center(
-            child: Text('No data found'),
+    return Consumer<UserProvider>(
+      builder: (context, value, child) {
+        final user = value.user as UserModel;
+        final preferences = value.preferences as UserPreferencesModel;
+        if (preferences.mobileData == false) {
+          return const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(),
+              Text("No se puede visualizar esta area sin conexión a internet"),
+            ],
           );
         } else {
-          return const Center(
-            child: CircularProgressIndicator(),
+          // TODO: save the local data in firestore
+
+          Future<List<ReportModel>> records =
+              ReportController().getReportsByUserId(user.email);
+
+          return FutureBuilder<List<ReportModel>>(
+            future: records,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                var femelles = 0;
+                var polls = 0;
+                var mascles = 0;
+                var indeterminat = 0;
+                var gossos = 0;
+                var gats = 0;
+                final _records = snapshot.data!;
+
+                for (final element in _records) {
+                  print(element.toJson());
+
+                  if (element.species != species) continue;
+
+                  femelles += element.females;
+                  polls += element.chickens;
+                  mascles += element.males;
+                  indeterminat += element.undetermined;
+                  gossos += element.dogs;
+                  gats += element.cats;
+                }
+
+                print("femelles ${femelles}");
+                print("polls ${polls}");
+                print("mascles ${mascles}");
+                print("indeterminats ${indeterminat}");
+                print("gossos ${gossos}");
+                print("gats ${gats}");
+
+                final data = [
+                  PieChartSectionData(
+                    value: femelles.toDouble(),
+                    color: kColorList['femelles'],
+                    radius: 50,
+                  ),
+                  PieChartSectionData(
+                    value: polls.toDouble(),
+                    color: kColorList['polls'],
+                    radius: 50,
+                  ),
+                  PieChartSectionData(
+                    value: mascles.toDouble(),
+                    color: kColorList['mascles'],
+                    radius: 50,
+                  ),
+                  PieChartSectionData(
+                    value: indeterminat.toDouble(),
+                    color: kColorList['indeterminat'],
+                    radius: 50,
+                  ),
+                  PieChartSectionData(
+                    value: gossos.toDouble(),
+                    color: kColorList['gossos'],
+                    radius: 50,
+                  ),
+                  PieChartSectionData(
+                    value: gats.toDouble(),
+                    color: kColorList['gats'],
+                    radius: 50,
+                  ),
+                ];
+
+                return SizedBox(
+                  width: 300,
+                  height: 300,
+                  child: PieChart(
+                    PieChartData(
+                      sections: data,
+                    ),
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                return const Center(
+                  child: Text('No data found'),
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
           );
         }
       },
