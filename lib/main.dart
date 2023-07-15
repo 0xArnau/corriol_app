558c150541a1bdaba7cef259b2cf529283045e8a
Arnau <>
2023-07-15 16:48:59 +0200

    refactor: change how to get the lang info

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'dart:async';
-
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:corriol_app/core/constants.dart';
 import 'package:corriol_app/core/notifiers.dart';
 import 'package:corriol_app/l10n/l10n.dart';
+import 'package:corriol_app/models/user_preferences_model.dart';
 import 'package:corriol_app/providers/user_provider.dart';
 import 'package:corriol_app/splash.dart';
 import 'package:flutter_config/flutter_config.dart';
@@ -24,89 +22,35 @@ void main() async {
 
   await FlutterConfig.loadEnvVariables();
 
-  UserProvider provider = UserProvider();
-
-  // runApp(const MyApp());
-  runApp(
-    ChangeNotifierProvider<UserProvider>.value(
-      value: provider,
-      child: const MyApp(),
-    ),
-  );
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({
-    super.key,
-  });
-
-  static late final Locale languageCode;
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late StreamSubscription<ConnectivityResult> subscription;
-  @override
-  void initState() {
-    super.initState();
-    // TODO: decide wether to move this to home page, widget_tree or somewhere else
-    subscription = Connectivity().onConnectivityChanged.listen(
-      (ConnectivityResult result) {
-        // To simulate there is no internet connection
-        // TODO: remove the next line in production
-        // result = ConnectivityResult.none; // For testing without connection
-        if (result == ConnectivityResult.none) {
-          isConnectedNotifier.value = false;
-          print('no internet connection');
-        } else if (result == ConnectivityResult.wifi) {
-          // internet connection with WiFi
-          // send the information to the database
-          print('internet connection: wifi');
-          isConnectedNotifier.value = true;
-        } else {
-          // some type of internet connection
-          // check this for more information: https://pub.dev/documentation/connectivity_plus/latest/
-          // TODO: decide which types are allowed to send information or not
-          // Don't send the information, save it locally
-          print("internet connection status: ${result}");
-          isConnectedNotifier.value = false;
-        }
-      },
-    );
-  }
-
-  // Be sure to cancel subscription after you are done
-  @override
-  dispose() {
-    subscription.cancel();
-    super.dispose();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print(userConfigNotifier.value.locale);
-    return ValueListenableBuilder(
-      valueListenable: userConfigNotifier,
-      builder: (context, userConfig, child) {
-        return MaterialApp(
-          title: 'Corriol APP',
-          theme: ThemeData(
-            primarySwatch: kColorPrimaryBlue,
-            useMaterial3: true,
-          ),
-          supportedLocales: L10n.all,
-          localizationsDelegates: const [
-            AppLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-          ],
-          home: const Splash(),
-          locale: userConfig.locale,
-        );
-      },
+    return ChangeNotifierProvider<UserProvider>(
+      create: (context) => UserProvider(),
+      child: Consumer<UserProvider>(
+        builder: (context, userProvider, child) {
+          final preferences = userProvider.preferences as UserPreferencesModel;
+          return MaterialApp(
+            title: 'Corriol APP',
+            theme: ThemeData(
+              primarySwatch: kColorPrimaryBlue,
+              useMaterial3: true,
+            ),
+            supportedLocales: L10n.all,
+            localizationsDelegates: const [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+            ],
+            home: const Splash(),
+            locale: preferences.lang, // Use the lang from UserPreferencesModel
+          );
+        },
+      ),
     );
   }
 }
