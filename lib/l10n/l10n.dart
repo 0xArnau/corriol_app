2b8ae596642a37f4e59888d51deb1fb861147dfc
Arnau <>
2023-07-28 17:06:29 +0200

    docs: add dartdocs for l10n

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+/// A class representing available locales for localization in the app.
 class L10n {
+  /// A list containing all available locales.
   static final all = [
     const Locale('en'),
     const Locale('es'),
