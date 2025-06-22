fa5325b74fac61c7a86424f21ab0eff10fe09b7d
Arnau <>
2025-06-22 12:01:58 +0200

    feat(card_widget): add semantics & exclude widgets from semantic tree

--- lib/widgets/card_widget.dart
+++ lib/widgets/card_widget.dart
@@ -18,37 +18,43 @@ class CardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) {
-          return page;
-        }));
-      },
-      child: Card(
-        color: background,
-        child: Container(
-          padding: const EdgeInsets.all(kDouble25),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Image(
-                  image: AssetImage(imagePath),
-                  width: 56.0,
-                  color: Colors.black,
-                ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Text(
-                    text,
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w700,
-                      color: foreground,
+    return Semantics(
+      button: true,
+      label: text,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return page;
+          }));
+        },
+        child: ExcludeSemantics(
+          child: Card(
+            color: background,
+            child: Container(
+              padding: const EdgeInsets.all(kDouble25),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Image(
+                      image: AssetImage(imagePath),
+                      width: 56.0,
+                      color: Colors.black,
                     ),
-                    textAlign: TextAlign.center,
-                  ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Text(
+                        text,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          color: foreground,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
